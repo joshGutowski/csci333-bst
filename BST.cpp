835edc07e6ec58172a97d551f56f85ae8d60273f
root <root@debian.rbh.rockynet.wireless.unca>
2012-11-02 19:25:37 -0400

    working on printTree

--- BST.cpp
+++ BST.cpp
@@ -1,8 +1,13 @@
 #include "BST.h"
 #include <iostream>
+#include <list>
+#include <vector>
+#include <iomanip>
+#include <math.h>
 
 using std::cout;
 using std::endl;
+using std::vector;
 
 template <typename T>
 BST<T>::BST() {
@@ -82,6 +87,83 @@ void BST<T>::traversalPrint(Node<T>* root) {
     traversalPrint(root->getLeftChild());
     std::cout << root->getValue() << std::endl;
     traversalPrint(root->getRightChild());
+  }
+}
+
+template <typename T>
+void BST<T>::treePrint(){
+  std::list<Node<T>* > s;
+  vector<vector<bool> > isPresent;
+  vector<vector<T> > levels;
+  vector<T> someRows;
+  vector<bool> someOtherRow;
+  levels.push_back(someRows);
+  isPresent.push_back(someOtherRow);
+
+  levels[0].push_back(root->getValue());
+  isPresent[0].push_back(true);
+
+  s.push_front(root);
+
+  int currentLevel = 1;
+  int nextLevel = 0;
+  int depth = 1;
+
+  while(!s.empty()){
+    Node<T>* val = s.front();
+    s.pop_front();
+
+    currentLevel--;
+
+    vector<T> row;
+    vector<bool> otherRow;
+    levels.push_back(row);
+    isPresent.push_back(otherRow);
+
+    if(val->getLeftChild() != 0){
+      s.push_back(val->getLeftChild());
+      levels[depth].push_back(val->getLeftChild()->getValue());
+      isPresent[depth].push_back(true);
+      nextLevel++;
+    }
+    else{
+      isPresent[depth].push_back(false);
+    }
+
+    if(val->getRightChild() != 0){
+      s.push_back(val->getRightChild());
+      levels[depth].push_back(val->getRightChild()->getValue());
+      isPresent[depth].push_back(true);
+      nextLevel++;
+    }
+    else{
+      isPresent[depth].push_back(false);
+    }
+    if(currentLevel == 0){
+      depth++;
+      currentLevel = nextLevel;
+      nextLevel = 0;
+    }
+  }
+  int width;
+
+  for(int i=0; i<depth; i++){
+    width = pow(1.625, depth-i-10);
+    if(isPresent[i][0]){
+      cout << std::setw(width) << levels[i][0];
+    }
+    else{
+      cout << std::setw(width) << " ";
+    }
+    for(int j=1; j < (int)levels[i].size(); j++){
+      if(isPresent[i][j]){
+	cout << std::setw(pow(1.75, depth-i-1)) << std::setfill(' ') << levels[i][j];
+      }
+      else{
+	cout << std::setw(pow(1.75, depth-i-1)) << std::setfill(' ') << " ";
+      }
+    }
+    cout << endl;
   }
 }
 
--- tree_test.cpp
+++ tree_test.cpp
@@ -7,21 +7,29 @@ using std::endl;
 int main() {
   BST<int>* bst = new BST<int>();
 
+  bst->insert(10);
   bst->insert(5);
-  bst->insert(6);
-  bst->insert(3);
-  bst->insert(2);
   bst->insert(7);
-  //bst->insert(12);
-
+  bst->insert(3);
+  bst->insert(8);
+  bst->insert(15);
+  bst->insert(18);
+  bst->insert(16);
+  bst->insert(12);
+  bst->insert(11);
+  bst->insert(17);
+  bst->treePrint();
 
   bst->print();
-  cout << "" << endl;
-  bst->remove(2);
+  cout << endl;
+  bst->remove(15);
 
   bst->print();
-  cout << "" << endl;
-  bst->remove(5);
+  cout << endl;
+  bst->remove(10);
 
+  bst->print();
+  cout << endl;
+  bst->remove(12);
   bst->print();
 }
